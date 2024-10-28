c01db8dbfa0b121211cc258085550d67d2ce6626
Choondrise <loboximax@gmail.com>
2024-10-28 01:19:57 +0100

    Remake UI

--- streamlit_klada.py
+++ streamlit_klada.py
@@ -2,12 +2,41 @@ import streamlit as st
 
 # Set page configuration
 st.set_page_config(page_title="Kladalytics", page_icon=":android:")
+# HTML/CSS to set the background GIF and title color
+markdown_settings = """
+<style>
+h1 {
+    color: green;  /* Set title color to green */
+}
+strong {
+    color: green;  /* Set bold text color to green */
+}
+</style>
+"""
+st.markdown(markdown_settings, unsafe_allow_html=True)
 
 # Display app name
 st.title("Kladalytics")
 
-# Display app description
-st.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod malesuada.")
+st.write("""
+Release Notes – Version 1.0
+
+Welcome to the first release of Kladalytics! 🎉 Your go-to platform for reliable sports betting tips from 
+certified tipsters. Here’s what you can expect in this initial version:
+
+**Key Features:**
+- **Certified Tipsters in One Place**: Access tips from verified and trusted tipsters across various sports.
+- **Diverse Sports Betting Options**: From football and basketball to niche sports, find tips for a wide range of games.
+- **Follow Your Favorite Tipsters**: Easily choose and follow tipsters whose insights match your preferences.
+- **Personalized Feed**: Get tips and updates directly in your feed from the tipsters you trust.
+
+**Why You’ll Love It:**
+- Comprehensive coverage of popular and emerging sports, with expert tips for each.
+- User-friendly and sleek design to make finding tips and navigating the app simple.
+- Regular updates and tips to keep you informed and ready for each game.
+
+This is just the start! Stay tuned for more features and improvements based on your feedback.
+""")
 
 # Path to your APK file
 apk_file_path = "./app-release.apk"
